fa37650ac47f95f141cf1216508f5b6fdc776194
Jhonny Frans Gallegos Mendoza <jgallegos@evolutionsoluciones.com>
2024-09-06 19:19:39 -0500

    aactulizando version kotlin

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,8 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:weatherappg9/models/weather_model.dart';
 import 'package:weatherappg9/services/api_services.dart';
 import 'package:weatherappg9/widgets/weather_item.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  WeatherModel? weatherModel;
+
+  Future<void> getDataLocation() async {
+    weatherModel = await ApiServices()
+        .getWeatherInfo(-11.993044302705213, -77.00924362697712);
+    setState(() {});
+  }
+
+  @override
+  void initState() {
+    getDataLocation();
+    // TODO: implement initState
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
@@ -31,80 +52,84 @@ class HomePage extends StatelessWidget {
           centerTitle: true,
           backgroundColor: Color(0xff272B2E),
         ),
-        body: Column(
-          children: [
-            Container(
-              padding: EdgeInsets.all(16),
-              margin: EdgeInsets.all(24),
-              width: double.infinity,
-              height: MediaQuery.of(context).size.height / 2,
-              decoration: BoxDecoration(
-                color: Colors.red,
-                borderRadius: BorderRadius.circular(25),
-                gradient: LinearGradient(
-                  colors: [
-                    Color(0xff225FF1),
-                    Color(0xff699BFC),
-                  ],
-                  begin: Alignment.topLeft,
-                  end: Alignment.bottomRight,
-                ),
-              ),
-              child: Column(
+        body: weatherModel == null
+            ? Center(
+                child: CircularProgressIndicator(),
+              )
+            : Column(
                 children: [
-                  Text(
-                    "Lima, Perú",
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 18,
+                  Container(
+                    padding: EdgeInsets.all(16),
+                    margin: EdgeInsets.all(24),
+                    width: double.infinity,
+                    height: MediaQuery.of(context).size.height / 2,
+                    decoration: BoxDecoration(
+                      color: Colors.red,
+                      borderRadius: BorderRadius.circular(25),
+                      gradient: LinearGradient(
+                        colors: [
+                          Color(0xff225FF1),
+                          Color(0xff699BFC),
+                        ],
+                        begin: Alignment.topLeft,
+                        end: Alignment.bottomRight,
+                      ),
+                    ),
+                    child: Column(
+                      children: [
+                        Text(
+                          "${weatherModel!.location.name}, ${weatherModel!.location.country}",
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 18,
+                          ),
+                        ),
+                        SizedBox(
+                          height: 16,
+                        ),
+                        Image.asset(
+                          "assets/images/heavycloudy.webp",
+                          height: 100,
+                        ),
+                        Text(
+                          "${weatherModel!.current.tempC}°",
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 100,
+                          ),
+                        ),
+                        Divider(
+                          color: Colors.white,
+                          height: 16,
+                        ),
+                        SizedBox(
+                          height: 16,
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceAround,
+                          children: [
+                            WeatherItem(
+                              image: "windspeed",
+                              unit: "km/h",
+                              value: weatherModel!.current.visKm.toString(),
+                            ),
+                            WeatherItem(
+                              image: "humidity",
+                              unit: "%",
+                              value: weatherModel!.current.humidity.toString(),
+                            ),
+                            WeatherItem(
+                              image: "cloud",
+                              unit: "%",
+                              value: weatherModel!.current.cloud.toString(),
+                            ),
+                          ],
+                        )
+                      ],
                     ),
                   ),
-                  SizedBox(
-                    height: 16,
-                  ),
-                  Image.asset(
-                    "assets/images/heavycloudy.webp",
-                    height: 100,
-                  ),
-                  Text(
-                    "23.9°",
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 100,
-                    ),
-                  ),
-                  Divider(
-                    color: Colors.white,
-                    height: 16,
-                  ),
-                  SizedBox(
-                    height: 16,
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceAround,
-                    children: [
-                      WeatherItem(
-                        image: "windspeed",
-                        unit: "km/h",
-                        value: "18",
-                      ),
-                      WeatherItem(
-                        image: "humidity",
-                        unit: "%",
-                        value: "76",
-                      ),
-                      WeatherItem(
-                        image: "cloud",
-                        unit: "%",
-                        value: "58",
-                      ),
-                    ],
-                  )
                 ],
               ),
-            ),
-          ],
-        ),
       ),
     );
   }
